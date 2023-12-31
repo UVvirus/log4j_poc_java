3f0abcfc5df5a9d809aa66d309823fdbfd450d97
Ubuntu <ubuntu@ip-172-31-20-164.us-west-2.compute.internal>
2023-12-31 06:59:13 +0000

    modified

--- java_log4j/python_applicaion/app.py
+++ java_log4j/python_applicaion/app.py
@@ -8,13 +8,15 @@ def home():
     if request.method == 'POST':
         # Get the user input from the form
         user_input = request.form['user_input']
+        user_input2=request.form['user_input2']
+        print(1)
 
         # Call the Java application using subprocess
-        result = subprocess.run(['java', 'LoggerApp', user_input], capture_output=True, text=True)
-
+        result = subprocess.run(['java', '-jar', "/home/ubuntu/log4j_poc_java/java_log4j/target/demo-1.0-SNAPSHOT-jar-with-dependencies.jar" , user_input, user_input2], capture_output=True, text=True)
+        print(result)
         # Get the output from the Java application
         java_output = result.stdout
-
+        print(java_output)
         return render_template('result.html', java_output=java_output)
     else:
         return render_template('index.html')
